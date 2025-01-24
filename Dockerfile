e455eea2d9aa50b8608e5c5a71843f25501b9f44
John Wregglesworth <wregglej@cyverse.org>
2025-01-24 13:39:00 -0700

    Add workflow-builder along with build changes

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,19 @@
 ### First stage
-FROM quay.io/goswagger/swagger as swagger
+FROM quay.io/goswagger/swagger AS swagger
+FROM harbor.cyverse.org/de/just:latest AS just
+FROM golang:1.23 AS build-root
 
-FROM golang:1.21 as build-root
+COPY --from=just /usr/local/cargo/bin/just /usr/bin/
 
 WORKDIR /go/src/github.com/cyverse-de/app-exposer
-COPY . . 
+
+COPY . .
 
 ENV CGO_ENABLED=0
 ENV GOOS=linux
 ENV GOARCH=amd64
 
-RUN make app-exposer
+RUN just app-exposer
 RUN cp ./bin/app-exposer /bin/app-exposer
 
 COPY --from=swagger /usr/bin/swagger /usr/bin/
---
+++ cmd/workflow-builder/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/cyverse-de/app-exposer/batch"
+	"github.com/cyverse-de/model/v6"
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	var (
+		err      error
+		inputJob model.Job
+
+		job                = flag.String("job", "", "The file containing the job definition. Required.")
+		transferImage      = flag.String("transfer-image", "harbor.cyverse.org/de/gocmd:latest", "(optional) Image used to transfer files to/from the data store")
+		transferWorkingDir = flag.String("transfer-working-dir", "/de-app-work", "The working directory within the file transfer image.")
+		transferLogLevel   = flag.String("transfer-log-level", "debug", "The log level of the output of the file transfer tool.")
+		statusSenderImage  = flag.String("status-sender-image", "harbor.cyverse.org/de/url-import:latest", "The image used to send status updates. Must container curl.")
+		analysisID         = flag.String("analysis-id", "", "The unique identifier for the analysis.")
+		out                = flag.String("out", "", "The file the workflow will be written to.")
+	)
+
+	flag.Parse()
+
+	if *job == "" {
+		log.Fatal("--job must be set.")
+	}
+
+	if *analysisID == "" {
+		log.Fatal("--analysis-id must be set.")
+	}
+
+	infile, err := os.Open(*job)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer infile.Close()
+
+	if err = json.NewDecoder(infile).Decode(&inputJob); err != nil {
+		log.Fatal(err)
+	}
+
+	opts := batch.BatchSubmissionOpts{
+		FileTransferImage:      *transferImage,
+		FileTransferWorkingDir: *transferWorkingDir,
+		FileTransferLogLevel:   *transferLogLevel,
+		StatusSenderImage:      *statusSenderImage,
+		AnalysisID:             *analysisID,
+	}
+
+	workflow := batch.NewWorkflow(&inputJob, &opts)
+
+	var outfile *os.File
+
+	if *out == "" {
+		outfile = os.Stdout
+	} else {
+		outfile, err := os.Create(*out)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer outfile.Close()
+	}
+
+	if err = yaml.NewEncoder(outfile).Encode(&workflow); err != nil {
+		log.Fatal(err)
+	}
+}
