edca435c7576ba1079ef4e48a6bcbc1d5906d28e
John Wregglesworth <wregglej@cyverse.org>
2025-06-23 13:50:58 -0700

    Added logic to send status updates and clean up requests from workflow-cleanup

--- cmd/workflow-cleanup/main.go
+++ cmd/workflow-cleanup/main.go
@@ -1,22 +1,119 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
 
+	"github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 	wfclientset "github.com/argoproj/argo-workflows/v3/pkg/client/clientset/versioned"
 	"github.com/cyverse-de/app-exposer/common"
+	"github.com/cyverse-de/app-exposer/constants"
+	"github.com/cyverse-de/app-exposer/httphandlers"
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func analysisStatus(appExposerURL, externalID string) (constants.AnalysisStatus, error) {
+	statusURL, err := url.JoinPath(appExposerURL, "info/analysis/status/by/external-id", externalID)
+	if err != nil {
+		return "", err
+	}
+	resp, err := http.Get(statusURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	var statusParsed httphandlers.AnalysisStatus
+	if err = json.Unmarshal(bodyBytes, &statusParsed); err != nil {
+		return "", err
+	}
+	return constants.AnalysisStatus(statusParsed.Status), nil
+}
+
+func sendStatus(statusURL, externalID, message string, state constants.AnalysisStatus) error {
+	body := map[string]string{
+		"job_uuid": externalID,
+		"hostname": "batch",
+		"message":  message,
+		"state":    string(state),
+	}
+
+	b, err := json.Marshal(&body)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(statusURL, "application/json", bytes.NewBuffer(b))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode >= 300 || resp.StatusCode < 200 {
+		return fmt.Errorf("status code %d: %s", resp.StatusCode, string(respBody))
+	}
+
+	return nil
+}
+
+func sendCleanupMessage(cleanUpURL, externalID string) error {
+	body := map[string]string{
+		"uuid": externalID,
+	}
+
+	b, err := json.Marshal(&body)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(cleanUpURL, "application/json", bytes.NewBuffer(b))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode >= 300 || resp.StatusCode < 200 {
+		return fmt.Errorf("status code %d: %s", resp.StatusCode, string(respBody))
+	}
+
+	return nil
+}
+
+func workflowStatusToDEStatus(workflowStatus v1alpha1.WorkflowPhase) constants.AnalysisStatus {
+	if workflowStatus == v1alpha1.WorkflowSucceeded {
+		return constants.Completed
+	}
+	return constants.Failed
+}
+
 func main() {
 	var (
-		err error
-		//cleanupURL = flag.String("cleanup-url", "http://webhook-eventsource-svc.argo-events/batch/cleanup", "The URL to POST to to clean up a workflow")
-		namespace = flag.String("namespace", "argo", "The namespace to use when looking up workflows")
+		err           error
+		cleanUpURL    = flag.String("cleanup-url", "http://webhook-eventsource-svc.argo-events/batch/cleanup", "The URL to POST to to clean up a workflow")
+		namespace     = flag.String("namespace", "argo", "The namespace to use when looking up workflows")
+		appExposerURL = flag.String("app-exposer-url", "http://app-exposer", "The URL to use when connecting to app-exposer")
+		setStatusURL  = flag.String("set-status-url", "http://webhook-eventsource-svc.argo-events/batch", "The URL to use when updating the status")
 	)
 
 	ctx := context.Background()
@@ -44,8 +141,38 @@ func main() {
 
 	for _, wf := range wfList.Items {
 		fmt.Printf("%s\t%s\n", wf.Status.Phase, wf.Name)
-	}
 
-	// If they're reported as failed, clean them up. Ignore the DE state
-	// it gets set before the workflow state is finalized.
+		// If the workflow is completed, make sure the status in the DE matches.
+		if wf.Status.Phase.Completed() {
+			fmt.Printf("%s is Completed according to Argo\n", wf.Name)
+
+			externalID := wf.ObjectMeta.Labels[constants.LabelKeyExternalID]
+			fmt.Printf("%s has an external ID of %s\n", wf.Name, externalID)
+
+			status, err := analysisStatus(*appExposerURL, externalID)
+			if err != nil {
+				log.Printf("%s\n", err)
+				continue
+			}
+			fmt.Printf("%s has a DE status of %s\n", wf.Name, status)
+
+			translatedStatus := workflowStatusToDEStatus(wf.Status.Phase)
+
+			if status != translatedStatus {
+				fmt.Printf("%s has a DE status of %s and it should be %s. Fixing...\n", wf.Name, status, translatedStatus)
+				if err = sendStatus(*setStatusURL, externalID, "setting status from workflow-cleanup", translatedStatus); err != nil {
+					log.Printf("%s\n", err)
+					continue
+				}
+				fmt.Printf("Done fixing the DE status for %s to %s\n", wf.Name, translatedStatus)
+			} else {
+				fmt.Printf("%s has matching statuses in the DE and Argo, sending clean up request...\n", wf.Name)
+				if err = sendCleanupMessage(*cleanUpURL, externalID); err != nil {
+					log.Printf("%s\n", err)
+					continue
+				}
+				fmt.Printf("Successfully sent a clean up request for %s\n", wf.Name)
+			}
+		}
+	}
 }
