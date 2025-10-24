d8f4f17f5b0238ea3c356574699eca346467cac7
John Wregglesworth <wregglej@arizona.edu>
2025-10-24 11:15:39 -0700

    Reformat swagger comments

--- httphandlers/timelimit.go
+++ httphandlers/timelimit.go
@@ -6,18 +6,18 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-//	@ID				time-limit-update
-//	@Summary		Handles requests to update the time limit on an already running VICE app.
-//	@Description	Updates the time limit on a running VICE app for a user. The user
-//	@Description	must have access to the analysis. The time limit is increased by a
-//	@Description	pre-configured amount.
-//	@Produce		json
-//	@Param			analysis-id	path		string	true	"Analysis ID"
-//	@Success		200			{object}	incluster.TimeLimit
-//	@Failure		400			{object}	common.ErrorResponse
-//	@Failure		403			{object}	common.ErrorResponse
-//	@Failure		500			{object}	common.ErrorResponse
-//	@Router			/vice/{analysis-id}/time-limit [post]
+// @ID				time-limit-update
+// @Summary		Handles requests to update the time limit on an already running VICE app.
+// @Description	Updates the time limit on a running VICE app for a user. The user
+// @Description	must have access to the analysis. The time limit is increased by a
+// @Description	pre-configured amount.
+// @Produce		json
+// @Param			analysis-id	path		string	true	"Analysis ID"
+// @Success		200			{object}	incluster.TimeLimit
+// @Failure		400			{object}	common.ErrorResponse
+// @Failure		403			{object}	common.ErrorResponse
+// @Failure		500			{object}	common.ErrorResponse
+// @Router			/vice/{analysis-id}/time-limit [post]
 func (h *HTTPHandlers) TimeLimitUpdateHandler(c echo.Context) error {
 	ctx := c.Request().Context()
 	log.Info("update time limit called")
@@ -52,15 +52,15 @@ func (h *HTTPHandlers) TimeLimitUpdateHandler(c echo.Context) error {
 
 }
 
-//	@ID				admin-time-limit-update
-//	@Summary		Updates the time limit on an analysis without requiring user information
-//	@Description	Updates the time limit on an analysis without requiring user information.
-//	@Produce		json
-//	@Param			analysis-id	path		string	true	"Analysis ID"
-//	@Success		200			{object}	incluster.TimeLimit
-//	@Failure		400			{object}	common.ErrorResponse
-//	@Failure		500			{object}	common.ErrorResponse
-//	@Router			/vice/admin/analyses/{analysis-id}/time-limit [post]
+// @ID				admin-time-limit-update
+// @Summary		Updates the time limit on an analysis without requiring user information
+// @Description	Updates the time limit on an analysis without requiring user information.
+// @Produce		json
+// @Param			analysis-id	path		string	true	"Analysis ID"
+// @Success		200			{object}	incluster.TimeLimit
+// @Failure		400			{object}	common.ErrorResponse
+// @Failure		500			{object}	common.ErrorResponse
+// @Router			/vice/admin/analyses/{analysis-id}/time-limit [post]
 func (h *HTTPHandlers) AdminTimeLimitUpdateHandler(c echo.Context) error {
 	ctx := c.Request().Context()
 	var (
@@ -87,17 +87,17 @@ func (h *HTTPHandlers) AdminTimeLimitUpdateHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, outputMap)
 }
 
-//	@ID				get-time-limit
-//	@Summary		Gets the time limit for an analysis on behalf of a user.
-//	@Description	Gets the time limit for an analysis on behalf of a user.
-//	@Produce		json
-//	@Param			user		query		string	true	"Username"
-//	@Param			analysis-id	path		string	true	"Analysis ID"
-//	@Success		200			{object}	incluster.TimeLimit
-//	@Failure		400			{object}	common.ErrorResponse
-//	@Failure		403			{object}	common.ErrorResponse
-//	@Failure		500			{object}	common.ErrorResponse
-//	@Router			/vice/{analysis-id}/time-limit [get]
+// @ID				get-time-limit
+// @Summary		Gets the time limit for an analysis on behalf of a user.
+// @Description	Gets the time limit for an analysis on behalf of a user.
+// @Produce		json
+// @Param			user		query		string	true	"Username"
+// @Param			analysis-id	path		string	true	"Analysis ID"
+// @Success		200			{object}	incluster.TimeLimit
+// @Failure		400			{object}	common.ErrorResponse
+// @Failure		403			{object}	common.ErrorResponse
+// @Failure		500			{object}	common.ErrorResponse
+// @Router			/vice/{analysis-id}/time-limit [get]
 func (h *HTTPHandlers) GetTimeLimitHandler(c echo.Context) error {
 	ctx := c.Request().Context()
 	log.Info("get time limit called")
@@ -135,15 +135,15 @@ func (h *HTTPHandlers) GetTimeLimitHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, outputMap)
 }
 
-//	@ID				admin-get-time-limit
-//	@Summary		Gets an analysis's time limit without requiring user info.
-//	@Description	Gets an analysis's time limit without requiring user info.
-//	@Produce		json
-//	@Param			analysis-id	path		string	true	"Analysis ID"
-//	@Success		200			{object}	incluster.TimeLimit
-//	@Failure		400			{object}	common.ErrorResponse
-//	@Failure		500			{object}	common.ErrorResponse
-//	@Router			/vice/admin/analyses/{analysis-id}/time-limit [get]
+// @ID				admin-get-time-limit
+// @Summary		Gets an analysis's time limit without requiring user info.
+// @Description	Gets an analysis's time limit without requiring user info.
+// @Produce		json
+// @Param			analysis-id	path		string	true	"Analysis ID"
+// @Success		200			{object}	incluster.TimeLimit
+// @Failure		400			{object}	common.ErrorResponse
+// @Failure		500			{object}	common.ErrorResponse
+// @Router			/vice/admin/analyses/{analysis-id}/time-limit [get]
 func (h *HTTPHandlers) AdminGetTimeLimitHandler(c echo.Context) error {
 	ctx := c.Request().Context()
 	log.Info("get time limit called")
--- httphandlers/updown.go
+++ httphandlers/updown.go
@@ -7,44 +7,44 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-//	@ID				trigger-downloads
-//	@Summary		Triggers input downloads for an analysis
-//	@Description	Triggers input downloads for an analysis
-//	@Produce		json
-//	@Param			id	path	string	true	"External ID"
-//	@Success		200
-//	@Failure		400	{object}	common.ErrorResponse
-//	@Failure		500	{object}	common.ErrorResponse
-//	@Router			/vice/{id}/download-input-files [post]
+// @ID				trigger-downloads
+// @Summary		Triggers input downloads for an analysis
+// @Description	Triggers input downloads for an analysis
+// @Produce		json
+// @Param			id	path	string	true	"External ID"
+// @Success		200
+// @Failure		400	{object}	common.ErrorResponse
+// @Failure		500	{object}	common.ErrorResponse
+// @Router			/vice/{id}/download-input-files [post]
 func (h *HTTPHandlers) TriggerDownloadsHandler(c echo.Context) error {
 	return h.incluster.DoFileTransfer(c.Request().Context(), c.Param("id"), constants.DownloadBasePath, constants.DownloadKind, true)
 }
 
-//	@ID				trigger-uploads
-//	@Summary		Triggers output uploads from an analysis
-//	@Description	Triggers output uploads from a running analysis.
-//	@Produce		json
-//	@Param			id	path	string	true	"External ID"
-//	@Success		200
-//	@Failure		400	{object}	common.ErrorResponse
-//	@Failure		500	{object}	common.ErrorResponse
-//	@Router			/vice/{id}/save-output-files [post]
+// @ID				trigger-uploads
+// @Summary		Triggers output uploads from an analysis
+// @Description	Triggers output uploads from a running analysis.
+// @Produce		json
+// @Param			id	path	string	true	"External ID"
+// @Success		200
+// @Failure		400	{object}	common.ErrorResponse
+// @Failure		500	{object}	common.ErrorResponse
+// @Router			/vice/{id}/save-output-files [post]
 func (h *HTTPHandlers) TriggerUploadsHandler(c echo.Context) error {
 	return h.incluster.DoFileTransfer(c.Request().Context(), c.Param("id"), constants.UploadBasePath, constants.UploadKind, true)
 }
 
-//	@ID				admin-trigger-downloads
-//	@Summary		Administratively trigger file downloads to an analysis
-//	@Description	Handles requests to trigger file downloads
-//	@Description	without requiring user information in the request and also operates from
-//	@Description	the analysis UUID rather than the external ID. For use with tools that
-//	@Description	require the caller to have administrative privileges.
-//	@Produce		json
-//	@Param			analysis-id	path	string	true	"Analysis ID"
-//	@Success		200
-//	@Failure		400	{object}	common.ErrorResponse
-//	@Failure		500	{object}	common.ErrorResponse
-//	@Router			/vice/admin/analyses/{analysis-id}/download-input-files [post]
+// @ID				admin-trigger-downloads
+// @Summary		Administratively trigger file downloads to an analysis
+// @Description	Handles requests to trigger file downloads
+// @Description	without requiring user information in the request and also operates from
+// @Description	the analysis UUID rather than the external ID. For use with tools that
+// @Description	require the caller to have administrative privileges.
+// @Produce		json
+// @Param			analysis-id	path	string	true	"Analysis ID"
+// @Success		200
+// @Failure		400	{object}	common.ErrorResponse
+// @Failure		500	{object}	common.ErrorResponse
+// @Router			/vice/admin/analyses/{analysis-id}/download-input-files [post]
 func (h *HTTPHandlers) AdminTriggerDownloadsHandler(c echo.Context) error {
 	var err error
 	ctx := c.Request().Context()
@@ -59,18 +59,18 @@ func (h *HTTPHandlers) AdminTriggerDownloadsHandler(c echo.Context) error {
 	return h.incluster.DoFileTransfer(ctx, externalID, constants.DownloadBasePath, constants.DownloadKind, true)
 }
 
-//	@ID				admin-trigger-uploads
-//	@Summary		Administratively trigger output file uploads from an analysis
-//	@Description	Handles requests to trigger file uploads without
-//	@Description	requiring user information in the request, while also operating from the
-//	@Description	analysis UUID rather than the external UUID. For use with tools that
-//	@Description	require the caller to have administrative privileges.
-//	@Produce		json
-//	@Param			analysis-id	path	string	true	"Analysis ID"
-//	@Success		200
-//	@Failure		400	{object}	common.ErrorResponse
-//	@Failure		500	{object}	common.ErrorResponse
-//	@Router			/vice/admin/analyses/{analysis-id}/save-output-files [post]
+// @ID				admin-trigger-uploads
+// @Summary		Administratively trigger output file uploads from an analysis
+// @Description	Handles requests to trigger file uploads without
+// @Description	requiring user information in the request, while also operating from the
+// @Description	analysis UUID rather than the external UUID. For use with tools that
+// @Description	require the caller to have administrative privileges.
+// @Produce		json
+// @Param			analysis-id	path	string	true	"Analysis ID"
+// @Success		200
+// @Failure		400	{object}	common.ErrorResponse
+// @Failure		500	{object}	common.ErrorResponse
+// @Router			/vice/admin/analyses/{analysis-id}/save-output-files [post]
 func (h *HTTPHandlers) AdminTriggerUploadsHandler(c echo.Context) error {
 	var err error
 	ctx := c.Request().Context()
